df84a2a72295b42f5acc6cdb0b7dc47bb531f62c
Kevin Chan <kevinykchan@gmail.com>
2020-06-30 20:18:46 -0400

    Updated comment

--- backend/src/api.py
+++ backend/src/api.py
@@ -11,16 +11,11 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-'''
 # db_drop_and_create_all()
 
 # ROUTES
 '''
-@TODO implement endpoint
+implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
@@ -40,7 +35,7 @@ def get_drinks():
 
 
 '''
-@TODO implement endpoint
+implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
@@ -63,7 +58,7 @@ def get_drinks_detail(payload):
 
 
 '''
-@TODO implement endpoint
+implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
@@ -96,7 +91,7 @@ def add_drink(jwt):
 
 
 '''
-@TODO implement endpoint
+implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -136,7 +131,7 @@ def update_drink(payload, drink_id):
 
 
 '''
-@TODO implement endpoint
+implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -178,7 +173,7 @@ def unprocessable(error):
 
 
 '''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
+implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
                     "success": False, 
@@ -197,7 +192,7 @@ def not_found(error):
 
 
 '''
-@TODO implement error handler for 400
+implement error handler for 400
     error handler should conform to general task above 
 '''
 @app.errorhandler(400)
@@ -210,7 +205,7 @@ def bad_request(error):
 
 
 '''
-@TODO implement error handler for AuthError
+mplement error handler for AuthError
     error handler should conform to general task above 
 '''
 @app.errorhandler(AuthError)
