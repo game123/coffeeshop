5ec47e93bf9fc7dfbf89abae01ee87d6a1601269
Kevin Chan <kevinykchan@gmail.com>
2020-06-30 20:15:59 -0400

    Submission commit

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+export const environment = {
+  production: false,
+  apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
+  auth0: {
+    url: "mycoffeeapi.us", // the auth0 domain prefix
+    audience: "Coffee", // the audience set for the auth0 app
+    clientId: "N2ID6A2fgXpJfpXu06YagA0Ygf020Gu9", // the client id generated for the auth0 app
+    callbackURL: "http://localhost:8100", // the base url of the running ionic application.
+  },
+};
